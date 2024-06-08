34a23398c522268924b0686482050fb6e8d74380
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-06-07 21:54:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+        function buscarUserInfo() {
+            const username = document.getElementById('usernameInput').value;
 
+            fetch(`https://api.github.com/users/${username}`)
+            .then(response => response.json())
+            .then(data => {
+                const nome = data.name || 'Nome não disponível';
+                const nomeUsuario = data.login;
+                const numRepositorios = data.public_repos;
+                const numSeguidores = data.followers;
+                const userInfoElement = document.getElementById('userInfo');
+                userInfoElement.innerHTML = `
+                    <p>Nome: ${nome}</p>
+                    <p>Nome de usuário: ${nomeUsuario}</p>
+                    <p>Número de repositórios públicos: ${numRepositorios}</p>
+                    <p>Número de seguidores: ${numSeguidores}</p>
+                `;
+            })
+            .catch(error => {
+                console.error('Ocorreu um erro ao buscar as informações do usuário:', error);
+            });
+        }
